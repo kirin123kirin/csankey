2265ba0fcd8a863416fc92dc8e36a9b7ce7687c8
kirin123 <3436009+kirin123kirin@users.noreply.github.com>
2021-11-26 15:07:50 +0900

    bug test

--- tools/scripts.py
+++ tools/scripts.py
@@ -32,9 +32,11 @@ def binary_always_allow():
         if not self.egg_info:
             return ""
 
-        print("hoge:", self.egg_info)
-        path = self._get_metadata_path(name)
-        print("foo:", path)
+        try:
+            path = self._get_metadata_path(name)
+        except AttributeError:
+            path = os.path.join(self.egg_info, *name.split("/"))
+        print("hoge:", path)
         value = self._get(path)
         try:
             return value.decode('utf-8')
